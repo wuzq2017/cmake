942c65ea3f207bfdbcc0af05f973eb27f017c2e3
wuzq <wdd_0227@aliyun.com>
2020-03-11 17:33:01 +0800

    add file options

--- log/log.c
+++ log/log.c
@@ -8,19 +8,31 @@
 #include <unistd.h>
 #include <string.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <errno.h>
 
+#define LOG_path "./log_dir"
+static FILE *fp1 = NULL;
+static pthread_mutex_t mutex_lock1 = PTHREAD_MUTEX_INITIALIZER;
+static int time_set_ok = 1;
+#define MAX_LOG_FILE_SIZE 10240
+static int flag1;
+static int num = 1;
 
 static key_t key1;
 
 static int msgid;
 typedef enum {
-MTYPE_invalid = 0,
+    MTYPE_invalid = 0,
     MTYPE_1,
-    MTYPE_2
+    MTYPE_2,
+    MTYPE_restart,
+    MTYPE_test
 }msg_type_t;
 
 typedef struct {
     msg_type_t mtype;
+    char *type_str;
     void (*p)(void *);
 }msg_proc_tbl_t;
 
@@ -33,12 +45,35 @@ typedef struct
     char dat[2048];
 } msg_t;
 
+static void write_file(char *str, unsigned int len)
+{
+    pthread_mutex_lock(&mutex_lock1);
+            
+    if (fp1 != NULL)
+        fwrite(str, len, 1, fp1);
+                
+    pthread_mutex_unlock(&mutex_lock1);
+} 
+static void log_save_test(void *param)
+{
+
+}
+static void log_save_restart(void *param)
+{
+
+    
+
+}
+
+
 msg_proc_tbl_t msg_proc_tbl[] = {
-    {MTYPE_invalid, NULL},
+
+    {MTYPE_test, "MTYPE_test",log_save_test},
+    {MTYPE_restart, "MTYPE_restart",log_save_restart},
+    {MTYPE_invalid, "",NULL},
 };
 
-#define LOG_path "~/log_dir"
-static FILE *fp1 = NULL;
+
 
 int init_file(void)
 {
@@ -56,7 +91,7 @@ int init_file(void)
     {
         if (mkdir(LOG_path, 0777))
         {
-            printf ("mkdir fail Error\n");
+            perror("mkdir fail Error 1");
             return -1;
         }
     }
@@ -65,7 +100,7 @@ int init_file(void)
     {
         if (mkdir(file, 0777))
         {
-            printf ("mkdir fail Error\n");
+            perror("mkdir fail Error 2");
             return -1;
         }
     }
@@ -81,7 +116,7 @@ int init_file(void)
         }
     }
     
-    while (dp = readdir(dirp))
+    while ((dp = readdir(dirp))!=NULL)
     {
         if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0)
             continue;
@@ -92,21 +127,14 @@ int init_file(void)
     return file_num;
 }
 
-static pthread_mutex_t mutex_lock1 = PTHREAD_MUTEX_INITIALIZER;
-static int time_set_ok = 1;
-#define MAX_LOG_FILE_SIZE 10240
-static int flag1;
-static int num = 1;
+
 void *make_file_thread(void *param)
 {
-    int j = 1;
     long i = 0;
     time_t tm;
     struct tm *tm_now;
     int file_num = 0;
     char file[100];
-    int file_creat = 1;
-    struct stat buf;
     
     while (1)
     {
@@ -136,9 +164,14 @@ void *make_file_thread(void *param)
                 file_num = n + 1;
                 time(&tm);
                 tm_now = localtime(&tm);
-                sprintf(file, "%s/%04d-%02d-%02d/%05d_%04d-%02d-%02d_02d_%02d_%02d.txt", 
-                        LOG_path, tm_now->tm_year + 1900, tm_now->tm_mon + 1, tm_now->tm_mday, 
-                        file_num, tm_now->tm_year + 1900, tm_now->tm_mon + 1, tm_now->tm_mday, tm_now->tm_hour, tm_now->tm_min, tm_now->tm_sec);
+                sprintf(file, "%s/%04d-%02d-%02d/%05d_%04d-%02d-%02d_%02d_%02d_%02d.txt", 
+                        LOG_path, tm_now->tm_year + 1900, tm_now->tm_mon + 1, tm_now->tm_mday,
+                        file_num,tm_now->tm_year + 1900, tm_now->tm_mon + 1, tm_now->tm_mday, 
+                        tm_now->tm_hour, tm_now->tm_min, tm_now->tm_sec);
+
+/* sprintf(file, "%s/%04d-%02d-%02d/%05d_%04d-%02d-%02d_02d_%02d_%02d.txt",  */
+/*                         LOG_path, tm_now->tm_year + 1900, tm_now->tm_mon + 1, tm_now->tm_mday,  */
+/*                         file_num, tm_now->tm_year + 1900, tm_now->tm_mon + 1, tm_now->tm_mday, tm_now->tm_hour, tm_now->tm_min, tm_now->tm_sec); */
                 printf("name :%s\n", file);
                 
                 do
@@ -159,18 +192,18 @@ void *make_file_thread(void *param)
                 while (NULL == fp1);
                 {
                     static int restart_f = 1;
-                if (restart_f == 1)
-                {
-                    msg_t msg;
-                    time(&tm);
-                    msg.mtype = MTYPE_1;
+                    if (restart_f == 1)
+                    {
+                        msg_t msg;
+                        time(&tm);
+                        msg.mtype = MTYPE_restart;
                     
-                    msg.num = num++;
-                    memset(msg.dat, 0, sizeof(msg.dat));
-                    sprintf(msg.dat, "%s\n", "###########start success##########");
-                    msgsnd(msgid, &msg, sizeof(msg) + strlen(msg.dat) - sizeof(msg.type) - sizeof(msg.dat) + 1, 0);
-                    restart_f  = 0;
-                }
+                        msg.num = num++;
+                        memset(msg.dat, 0, sizeof(msg.dat));
+                        sprintf(msg.dat, "%s\n", "###########start success##########");
+                        msgsnd(msgid, &msg, sizeof(msg) + strlen(msg.dat) - sizeof(msg.mtype) - sizeof(msg.dat) + 1, 0);
+                        restart_f  = 0;
+                    }
                 }
             }
         }
@@ -249,7 +282,8 @@ static void msg_q_create(void)
 
 static void thread_init(void)
 {
-    pthread_t tid;
+    pthread_t tid,tid1;
+    pthread_create(&tid1,NULL,make_file_thread,NULL);
     pthread_create(&tid,NULL,log_save_proc,NULL);
 }
 void log_init(void)
--- main.c
+++ main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include "math/math.h"
 #include "log/log.h"
+#include <unistd.h>
 
 int main(int argc, char **argv)
 {
-  int a,b,c;
+    int a,b,c;
 
-  log_init();
+    log_init();
 
-  a = 10;
-  b =11;
-  c = add(a,b);
-  printf("sum is %d, diff is %d\n",c,dec(a,b));
-  return 0;
+    a = 10;
+    b =11;
+    c = add(a,b);
+    printf("sum is %d, diff is %d\n",c,dec(a,b));
+    while(1) {
+        sync();
+        sleep(5);
+    }
+
+    return 0;
 }
